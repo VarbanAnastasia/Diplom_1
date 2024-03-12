f10c18df7609288ef03e9290c881c24ea4782c23
VarbanAnastasia <145008875+VarbanAnastasia@users.noreply.github.com>
2024-03-12 17:34:33 +0300

    убран лишний мок

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,5 +1,3 @@
-from unittest.mock import Mock
-
 from praktikum.bun import Bun
 
 
@@ -11,15 +9,5 @@ class TestBun:
 
     def test_get_price(self):
         bun = Bun("test", 10)
-        assert bun.get_price() == 10
-
-    def test_mock_bun_get_name(self):
-        bun = Mock()
-        bun.get_name.return_value = "bulka"
-        assert bun.get_name() == "bulka"
-
-    def test_mock_bun_get_price(self):
-        bun = Mock()
-        bun.get_price.return_value = 10
         assert bun.get_price() == 10
 
